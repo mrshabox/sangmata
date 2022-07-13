b7cd6405c246e0e9423b7310abcd50c0ab702bbd
mrshabox <106615216+mrshabox@users.noreply.github.com>
2022-07-14 02:48:35 +0700

    Update sangmata.py

--- sangmata.py
+++ sangmata.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from telegram import Update,KeyboardButton,ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler,CallbackContext, MessageHandler,Filters
 import os
-PORT = int(os.environ.get("PORT", 3978)) 
+PORT = int(os.environ.get('PORT','8443'))
 TOKEN = os.environ.get('BOT_TOKEN',None)
 HEROKU_APP_NAME=os.environ.get('HEROKU_APP_NAME',None)
 owner=os.environ.get('OWNER',None)
@@ -42,15 +42,15 @@ updater=Updater(TOKEN)
 dispatcher= updater.dispatcher
 
 
-#############################№##################################№#
-def start(update: Update, context: CallbackContext):
+#############################â„–##################################â„–#
+def start(update,context):
   logg(update.message)
   update.message.reply_text("Forward any message to this chat to see user history.")
-#############################№##################################№#
+#############################â„–##################################â„–#
 
 
 
-#############################№##################################№#
+#############################â„–##################################â„–#
 def Forwarded(update, context):
   logg(update.message)
   message= update.message
@@ -58,16 +58,16 @@ def Forwarded(update, context):
     user=message.forward_from
     message.reply_text(f"""
 Name History
-👤 {user.id}
+ðŸ‘¤ {user.id}
 
 1. [{ran_date()}] {user.full_name}
 """)
-#############################№##################################№#
+#############################â„–##################################â„–#
 
 
 
   
-#############################№##################################№#
+#############################â„–##################################â„–#
 def search_id(update,context):
   logg(update.message)
   message= update.message
@@ -77,20 +77,20 @@ def search_id(update,context):
     user=context.bot.getChat(id_search)
     message.reply_text(f"""
 Name History
-👤 {user.id}
+ðŸ‘¤ {user.id}
 
 1. [{ran_date()}] {user.full_name}
 """)
   except Exception as e:
     print(e)
     message.reply_text("No records found")
-#############################№##################################№#
+#############################â„–##################################â„–#
 
 
 
 
 
-#############################№##################################№#
+#############################â„–##################################â„–#
 def check_name(update,context):
   logg(update.message)
   message=update.message
@@ -102,17 +102,17 @@ def check_name(update,context):
     mesg=message
   text=f"""
 Name History
-👤 {user.id}
+ðŸ‘¤ {user.id}
 
 1. [{ran_date()}] {user.full_name}
   """
   mesg.reply_text(text)
-#############################№##################################№#
+#############################â„–##################################â„–#
 
 
 
 
-#############################№##################################№#
+#############################â„–##################################â„–#
 def check_brain(update,context):
   logg(update.message)
   message=update.message
@@ -130,11 +130,11 @@ def check_brain(update,context):
     user=message.from_user
     #msg_id= message.message_id
     print(message.reply_text(f"No Brain Found"))
-#############################№##################################№#
+#############################â„–##################################â„–#
 
 
 
-#############################№##################################№#
+#############################â„–##################################â„–#
 def check_username(update,context):
   logg(update.message)
   message=update.message
@@ -147,44 +147,58 @@ def check_username(update,context):
   try:
     text=f"""
 Username History
-👤 {user.id}
+ðŸ‘¤ {user.id}
 
 1. [{ran_date()}] {user.username}
 """
   except:
     text=f"""
 Username History
-👤 {user.id}
+ðŸ‘¤ {user.id}
 
 1. [{ran_date()}] (No Username)
   """
   mesg.reply_text(text)
-#############################№##################################№#
+#############################â„–##################################â„–#
 
 
 
 
 
-#############################№##################################№#
+#############################â„–##################################â„–#
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
-#############################№##################################№#
+#############################â„–##################################â„–#
 
 
 
   
 
 
-def main():
-  dispatcher.add_handler(MessageHandler(Filters.chat_type.private & Filters.forwarded,Forwarded))
-  dispatcher.add_handler(CommandHandler("start",start))
-  dispatcher.add_handler(CommandHandler("search_id",search_id))
-  dispatcher.add_handler(CommandHandler("check_name",check_name))
-  dispatcher.add_handler(CommandHandler("check_username",check_username))
-  dispatcher.add_handler(CommandHandler("check_brain",check_brain))
-  dispatcher.add_handler(MessageHandler(Filters.chat_type.private,start))
-  dispatcher.add_error_handler(error)
-  updater.start_polling()
-  updater.idle()
-main()
+
+dispatcher.add_handler(MessageHandler(Filters.chat_type.private & Filters.forwarded,Forwarded))
+dispatcher.add_handler(CommandHandler("start",start))
+dispatcher.add_handler(CommandHandler("search_id",search_id))
+dispatcher.add_handler(CommandHandler("check_name",check_name))
+dispatcher.add_handler(CommandHandler("check_username",check_username))
+dispatcher.add_handler(CommandHandler("check_brain",check_brain))
+dispatcher.add_handler(MessageHandler(Filters.chat_type.private,start))
+dispatcher.add_error_handler(error)
+
+
+updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+updater.bot.setWebhook('https://'+HEROKU_APP_NAME+'.herokuapp.com/' + TOKEN)
+updater.start_webhook(listen="0.0.0.0",
+
+                      port=PORT,
+
+                      url_path=TOKEN,
+
+                      webhook_url="https://"+HEROKU_APP_NAME+".herokuapp.com/" + TOKEN)
+
+updater.idle()
+
+updater.idle()
